b938603ff3cef8c831c66b2891f8777926af235b
Mike Bauer <mbauer@avatarpartners.com>
2021-06-22 16:44:35 -0400

    added voice basics

---
+++ Assets/Scripts/VoiceCommands.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class VoiceCommands : MonoBehaviour
+{
+    #region Voice Commands
+    public void VC_MainMenu()
+    {
+        Debug.Log("You said: Main Menu...");
+    }
+
+    #endregion
+}
