24c3ebca59fc331ce3a4c3cf9d5d7d62a21d0d46
Mike Bauer <mbauer@avatarpartners.com>
2021-06-23 13:57:08 -0400

    Added some audio and basic UI

--- Assets/Scripts/VoiceCommands.cs
+++ Assets/Scripts/VoiceCommands.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
 
 public class VoiceCommands : MonoBehaviour
 {
+
+    public MixedRealitySpeechCommandsProfile profile;
+
     #region Voice Commands
     public void VC_MainMenu()
     {
         Debug.Log("You said: Main Menu...");
+
     }
 
     #endregion
